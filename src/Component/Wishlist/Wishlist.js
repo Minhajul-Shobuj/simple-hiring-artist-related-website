adfe6e410d1569a37cc4aba793d49015455df711
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-09-26 14:38:12 +0600

    third

--- src/Component/Wishlist/Wishlist.js
+++ src/Component/Wishlist/Wishlist.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import './Wishlist.css'
 
 const Wishlist = (props) => {
-    console.log(props.cart)
     const cart = props.cart;
     let name = '';
     let demand = 0;
+    let addedArtist = 0;
     for (const element of cart) {
-        if (name !== element.name) {
+        if (name.indexOf(element.name) === -1) {
             name = name + element.name + ',';
+            demand = Number.parseInt(demand) + Number.parseInt(element.Demand);
+            addedArtist = addedArtist + 1;
         }
-        demand = Number.parseInt(demand) + Number.parseInt(element.Demand);
+
     }
     return (
-        <div>
-            <h3>Whislist</h3>
-            <p> Artist name:{name}</p>
-            <p> Artist demend: {(demand)}</p>
+        <div className="wishlist">
+            <h3>Whislist</h3><hr />
+            <p>Added Artist: {addedArtist}</p>
+            <ul> Artist name: <li>{name}</li> </ul>
+            <p>Total of Artist demend:  {demand}Tk</p>
         </div>
     );
 };
